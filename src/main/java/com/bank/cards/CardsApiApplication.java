2c4eef6def10bc5e5ef298428c20535276d7c1a9
Jose Antonio Alvino Velasque <jalvinov@everis.com>
2021-01-06 11:57:23 -0500

    Swagger implemented

--- src/main/java/com/bank/cards/CardsApiApplication.java
+++ src/main/java/com/bank/cards/CardsApiApplication.java
@@ -1,13 +1,25 @@
 package com.bank.cards;
 
+import com.bank.cards.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class CardsApiApplication {
-
+@Import(SwaggerConfiguration.class)
+public class CardsApiApplication implements WebMvcConfigurer
+{
 	public static void main(String[] args) {
 		SpringApplication.run(CardsApiApplication.class, args);
 	}
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry)
+	{
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+	}
 }
--- src/main/java/com/bank/cards/controller/CardController.java
+++ src/main/java/com/bank/cards/controller/CardController.java
@@ -3,6 +3,10 @@ package com.bank.cards.controller;
 import com.bank.cards.entity.CardResponse;
 import com.bank.cards.service.CardService;
 import io.reactivex.Single;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/core/cards")
+@Api("/core/cards")
 public class CardController
 {
     @Autowired
     private final CardService cardService;
 
     @GetMapping
+    @ApiOperation(value = "Find Cards by Document Number", notes = "Retrieving a collection of cards", response = CardResponse.class)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", response = CardResponse.class)
+    })
     public Single<com.bank.cards.entity.CardResponse> getAllCardsByDocumentNumber(@RequestParam(name = "documentNumber") String documentNumber)
     {
         return cardService.getAllCardsByDocument(documentNumber);
